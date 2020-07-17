eae8f4aa34abda2eba4e23cc358470248494778f
micktseng <mick@truney.com>
2020-07-17 16:49:00 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,1 @@
+此模組依賴 OCA 的 [base_location](https://github.com/OCA/partner-contact/tree/13.0/base_location) 模組，可以使用郵遞區號(Zip)自動填入縣市和鄉政市區
---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2016 Nicolas Bessi, Camptocamp SA
+# Copyright 2018-2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Location management for Taiwan",
+    "version": "13.0.1.1.0",
+    "depends": ["base_location"],
+    "author": "Mick Tseng, Odoo Taiwan",
+    "license": "AGPL-3",
+    "summary": """台灣地址相關功能強化""",
+    "website": "https://github.com/OCA/partner-contact",
+    "data": ["data/res.city.csv", "data/res.city.zip.csv"],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
